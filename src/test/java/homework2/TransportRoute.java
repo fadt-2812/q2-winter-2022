8d8cb6e3c0eab3caa922f8b58e09960c662c5bd5
Fadt <fadt2812@gmail.com>
2022-10-19 20:58:52 +0300

    initial commit

---
+++ src/test/java/homework2/TransportRoute.java
@@ -0,0 +1,15 @@
+package homework2;
+
+public class TransportRoute {
+
+    private String route1;
+    private String route2;
+    private String route3;
+    private double autoTank;
+    private double locomotiveTank;
+    private double airplaneTank;
+    private double autoFuelConsumption;
+    private double loco
+
+}
+
