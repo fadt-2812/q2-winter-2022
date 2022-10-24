6ba65d78b14685ed7e06e41013a196f7bd5de9a8
Fadt <fadt2812@gmail.com>
2022-10-24 10:41:42 +0300

    initial commit

--- src/test/java/homework2/TransportRoute.java
+++ src/test/java/homework2/TransportRoute.java
@@ -2,14 +2,77 @@ package homework2;
 
 public class TransportRoute {
 
-    private String route1;
-    private String route2;
-    private String route3;
-    private double autoTank;
-    private double locomotiveTank;
-    private double airplaneTank;
-    private double autoFuelConsumption;
-    private double loco;
+    private String transport;
+    private String route;
+    private int distance;
+    private double tankLitr;
+    private double fuelConsumptionHundredKm;
+    private double literRoute;
+    private double kmOneTank;
+    private boolean oneTankFuel;
 
+    public String getTransport() {
+        return transport;
+    }
+
+    public void setTransport(String transport) {
+        this.transport = transport;
+    }
+
+    public String getRoute() {
+        return route;
+    }
+
+    public void setRoute(String route) {
+        this.route = route;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public void setDistance(int distance) {
+        this.distance = distance;
+    }
+
+    public double getLiterRoute() {
+        return literRoute;
+    }
+
+    public void setLiterRoute(double literRoute) {
+        this.literRoute = literRoute;
+    }
+
+    public double getKmOneTank() {
+        return kmOneTank;
+    }
+
+    public void setKmOneTank(double kmOneTank) {
+        this.kmOneTank = kmOneTank;
+    }
+
+    public double getTankLitr() {
+        return tankLitr;
+    }
+
+    public void setTankLitr(double tankLitr) {
+        this.tankLitr = tankLitr;
+    }
+
+    public double getFuelConsumptionHundredKm() {
+        return fuelConsumptionHundredKm;
+    }
+
+    public void setFuelConsumptionHundredKm(double fuelConsumptionHundredKm) {
+        this.fuelConsumptionHundredKm = fuelConsumptionHundredKm;
+    }
+
+    public boolean isOneTankFuel() {
+        return oneTankFuel;
+    }
+
+    public void setOneTankFuel(boolean oneTankFuel) {
+        this.oneTankFuel = oneTankFuel;
+    }
 }
 
