9f01e63ff8516e1f669068d93661628e69927e2d
Fadt <fadt2812@gmail.com>
2022-11-12 12:29:05 +0200

    My search - Latvia

--- src/test/java/homework3/MySelenTestLatvia.java
+++ src/test/java/homework3/MySelenTestLatvia.java
@@ -8,12 +8,14 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.sql.Driver;
 import java.time.Duration;
 import java.util.List;
 
 public class MySelenTestLatvia {
 
-    private final By ACCEPT_COOKIES_BTN = By.id("onetrust-accept-btn-handler");
+        private final By ACCEPT_COOKIES_BTN = By.id("onetrust-accept-btn-handler");
+
     @Test
     public void openHomePageCheck() {
         System.setProperty("webdriver.chrome.driver", "c://chromedriver.exe");
@@ -24,26 +26,20 @@ public class MySelenTestLatvia {
         WebDriverWait wait = new WebDriverWait(browser, Duration.ofSeconds(10));
 
         wait.until(ExpectedConditions.elementToBeClickable(ACCEPT_COOKIES_BTN));
-        browser.findElement(ACCEPT_COOKIES_BTN).click();}
-    @Test
-    public void countryLatvia() {
-        System.setProperty("webdriver.chrome.driver", "c://chromedriver.exe");
-        WebDriver browser = new ChromeDriver();
-        browser.manage().window().maximize();
-        browser.get("http://www.discovercars.com/");
-
-        WebDriverWait wait = new WebDriverWait(browser, Duration.ofSeconds(10));
-
-        wait.until(ExpectedConditions.elementToBeClickable(ACCEPT_COOKIES_BTN));
         browser.findElement(ACCEPT_COOKIES_BTN).click();
-        browser.findElements(By.xpath("//tl-box(text()='latvia'"));
+        browser.findElement(By.xpath(".//div[@class = 'tl-box'][28]/a")).click();
 
-
-
+    }
 
 
 
     }
 
 
-}
+
+
+
+
+
+
+
